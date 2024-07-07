6593d36e1690d79d1ee6c1867318da6ef7ca99dd
abraham <abrahamtikuye@gmail.com>
2024-07-07 15:10:14 +0300

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  "scripts": {
+    "dev": "next dev"
+  },
   theme: {
     extend: {
       backgroundImage: {
